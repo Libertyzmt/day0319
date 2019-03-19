654180b4c9ce5c03554d75d67873a62734235d27
赵猛彤 <1183525873@qq.com>
2019-03-19 21:49:35 +0800

    初级增删改查

--- counter/urls.py
+++ counter/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-
+    path('', views.select, name='select'),
+    path('select2/<int:id>/', views.post, name='post'),
+    path('remove/<int:id>/', views.removes, name='removes'),
+    path('update/<int:pk>/', views.ArticleUpdate.as_view(), name='update')
 ]
--- counter/views.py
+++ counter/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
+def select(request):
+    selects = Article.objects.all()
+    return render(request, 'post/article_select.html', {'selects': selects})
+
+
+def post(request, id):
+    post = Article.objects.get(id=id)
+    return render(request, 'post/article_select2.html', {'post': post})
+
+class ArticleUpdate(UpdateView):
+    model = Article
+    fields = ('title','body')
+    template_name = 'post/article_create.html'
+    success_url = reverse_lazy('select')
+
+
+def removes(request, id):
+    remove = Article.objects.get(id=id)
+    remove.delete()
+    return redirect("select")
